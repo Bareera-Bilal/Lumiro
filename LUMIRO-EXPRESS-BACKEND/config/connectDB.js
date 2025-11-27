cff4cb0450e2fee6240207a7165ab082daa07b53
Bareera Bilal <bareerabilal03@gmail.com>
2025-11-27 22:26:34 +0530

    Add files via upload

--- LUMIRO-EXPRESS-BACKEND/config/connectDB.js
+++ LUMIRO-EXPRESS-BACKEND/config/connectDB.js
@@ -6,7 +6,6 @@ const connectDb = async () => {
 
     try {
         const connectionString ="mongodb+srv:Lumiro:<db_password>@cluster0.b9qbr1l.mongodb.net/Lumiro=Cluster0?retryWrites=true&w=majority&appName=DotnetWebAPI"
-// "mongodb+srv://irfanusuf33:dotnetwebapi@dotnetwebapi.m48gol1.mongodb.net/Trinkle?retryWrites=true&w=majority&appName=DotnetWebAPI"
         await mongoose.connect(connectionString)  
         console.log("Db Connected ")   
     } catch (error) {
--- LUMIRO-EXPRESS-BACKEND/config/nodemailer.js
+++ LUMIRO-EXPRESS-BACKEND/config/nodemailer.js
@@ -2,27 +2,17 @@ const nodeMailer = require("nodemailer")
 
 require('dotenv').config()  
 
-// const transporter = nodeMailer.createTransport({
-//     host : process.env.SMTP_HOST,
-//     port : 587,
-//     secure : false ,      // ONLY TRUE FOR 465
-//     auth : {
-//         user: process.env.SMTP_USER ,
-//         pass : process.env.SMTP_PASS     
-//     }
-// })
+//TEST ACCOUNT
+const transporter = nodeMailer.createTransport({
+    host : process.env.SMTP_HOST,
+    port : 587,
+    secure : false ,      
+    auth : {
+        user: process.env.SMTP_USER ,
+        pass : process.env.SMTP_PASS     
+    }
+})
 
 
-
-// Create a test account 
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
-  },
-});
 
 module.exports = {transporter}
